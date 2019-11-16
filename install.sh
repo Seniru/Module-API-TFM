cebfa409af129127225399b1f43275f3d0f53d5a
Seniru Pasan Indira <senirupasan@gmail.com>
2019-11-16 10:24:41 +0000

    chore: Added script for installing luadist and modules
    This commit introduces important changes for developers
     - We introduced install script for installing required resources.
     They contain,
      * luadist (package manager for lua)
     * busted (test framwork for lua)
     * luacov (coverage for lua)

---
+++ install.sh
@@ -0,0 +1,11 @@
+#pre-installation
+echo "$(curl -fksSL https://tinyurl.com/luadist)" | bash
+alias lua='LuaDist/bin/lua'
+alias luadist='LuaDist/bin/luadist'
+
+#install required libraries
+luadist install busted
+luadist install luacov
+
+alias busted='LuaDist/bin/busted'
+alias luacov='LuaDist/bin/luacov'

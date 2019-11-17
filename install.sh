7a103211e669f376a7d70e0b0ab19972cd4aed73
Seniru Pasan Indira <senirupasan@gmail.com>
2019-11-17 11:56:40 +0000

    fix: Specified target shell
    This commit is related to issue #2.
    
    closes #2

--- install.sh
+++ install.sh
@@ -1,11 +1,13 @@
+#!/bin/sh
+
 #pre-installation
 echo "$(curl -fksSL https://tinyurl.com/luadist)" | bash
 alias lua='LuaDist/bin/lua'
 alias luadist='LuaDist/bin/luadist'
 
 #install required libraries
-luadist install busted
-#luadist install luacov
+#luadist install busted
+luadist install luacov
 
 #alias busted='LuaDist/bin/busted'
 alias luacov='LuaDist/bin/luacov'
